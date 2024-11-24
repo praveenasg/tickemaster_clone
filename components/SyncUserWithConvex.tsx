dab9bdc259a8e4705050f8e6bf73a00362e32d6a
praveenasg <praveenasg98@gmail.com>
2024-11-23 19:06:12 -0500

    Created Schema, header, sync users with convex and clerk.

---
+++ components/SyncUserWithConvex.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+function SyncUserWithConvex() {
+  const { user } = useUser();
+  // update user
+  const updateUser = useMutation(api.users.updateUser);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const syncUser = async () => {
+      try {
+        await updateUser({
+          userId: user.id,
+          name: `${user.firstName ?? ""} ${user.lastName ?? ""}`.trim(),
+          email: user.emailAddresses[0]?.emailAddress ?? "",
+        });
+      } catch (error) {
+        console.error("Error syncing user", error);
+      }
+    };
+    syncUser();
+  }, [user, updateUser]);
+  return null;
+}
+export default SyncUserWithConvex;
